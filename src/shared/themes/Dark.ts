31bb4d2d546d12db2c8ec860e841edcadb6dfd6b
Gabriel Rodrigues <gabriel.goes@radixeng.com.br>
2024-03-16 16:48:24 -0300

    feat: adicionado tema escuro e troca de tema

---
+++ src/shared/themes/Dark.ts
@@ -0,0 +1,22 @@
+import { createTheme } from "@mui/material";
+
+export const DarkTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#0e4686",
+      dark: "#1565c0",
+      light: "#4383cc",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#790e3c",
+      dark: "#ad1457",
+      light: "#bd4378",
+      contrastText: "#000",
+    },
+    background: {
+      default: "#00838f",
+      paper: "#00acc1",
+    },
+  },
+});
