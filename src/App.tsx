93a310958375b54e391f2bf9699e0b4eb39792f9
Gabriel Rodrigues <gabriel.goes@radixeng.com.br>
2024-03-18 10:36:44 -0300

    feat: adicionado axios react-query e interceptors

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
-import { AppThemeProvider, AppDrawerProvider } from "./shared/contexts";
-import { MenuLateral } from "./shared/components";
+import {
+  AppThemeProvider,
+  AppDrawerProvider,
+  AuthProvider,
+} from "./shared/contexts";
+import { ButtonTheme, DrawerMenu } from "./shared/components";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./shared/services";
+import { Login } from "./pages";
 
 export const App = () => {
   return (
-    <AppThemeProvider>
-      <AppDrawerProvider>
-        <BrowserRouter>
-          <MenuLateral>
-            <AppRoutes />
-          </MenuLateral>
-        </BrowserRouter>
-      </AppDrawerProvider>
-    </AppThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <AppThemeProvider>
+          <AppDrawerProvider>
+            <Login>
+              <BrowserRouter>
+                <DrawerMenu>
+                  <AppRoutes />
+                </DrawerMenu>
+                <ButtonTheme />
+              </BrowserRouter>
+            </Login>
+          </AppDrawerProvider>
+        </AppThemeProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
---
+++ src/shared/services/api/AxiosConfig/index.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { errorInterceptor, responseInterceptor } from "./Interceptors";
+import { Environment } from "../../../environment";
+
+const Api = axios.create({
+  baseURL: Environment.URL_BASE,
+  // headers: {
+  //   Authorization: `Bearer ${JSON.parse(
+  //     localStorage.getItem("APP_ACCESS_TOKEN") || ""
+  //   )}`,
+  // },
+});
+
+Api.interceptors.response.use(
+  (response) => responseInterceptor(response),
+  (error) => errorInterceptor(error)
+);
+
+export { Api };
